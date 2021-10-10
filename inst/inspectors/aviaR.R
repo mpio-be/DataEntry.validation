dee025b5549b6f630d60ab0e3a42d7e7217b65a2
mihai <valcu@orn.mpg.de>
2021-10-10 19:45:37 +0200

    -

--- inst/inspectors/aviaR.R
+++ inst/inspectors/aviaR.R
@@ -1,26 +1,26 @@
 #' ==========================================================================
-#' validators used by aviaR package. 
-#' require(wadeR)
+#' validators used by aviaR package.
+#' require(aviaR)
 #' require(glue)
 #' require(stringr)
 #' require(foreach)
 #' require(DataEntry.validation)
-#' 
-#' 
+#'
+#'
 #' tabs = c('ADULTS', 'CHICKS', 'AVIARIES', 'FOUNDERS')
-#' 
+#'
 #' o = foreach(i = 1:length(tabs), .combine = rbind) %do% {
-#' 
+#'
 #'  x = sdb::dbq(q = glue('select * from {tabs[i]} ') )
 #'  class(x) = c(class(x), tabs[i] )
-#'  
+#'
 #'  inspector_loader(package = 'aviaR')
-#'  
+#'
 #'  ii = inspector(x)
 #'  evalidators(ii)[, table :=  tabs[i] ]
-#'  
+#'
 #'  }
-#' 
+#'
 #'
 #' ==========================================================================
 
@@ -29,45 +29,32 @@ inspector.ADULTS <- function(x, ...){
 
   list(
 
-  # GENERAL #################################  
+  # GENERAL #################################
     # Mandatory to enter
-    x[, .(ID,av_ID, author, date, time)] %>% 
+    x[, .(ID,av_ID, author, date, time)] %>%
     is.na_validator,
 
      # Correct format?
     x[, .(date)]  %>% date_validator(),
     x[, .(time)]  %>% hhmm_validator(),
 
-      x[ , .(author)] %>% 
-    is.element_validator(v = data.table(variable = "author", 
-    set = list(dbq(q = "SELECT initials ii FROM RUFFatSEEWIESEN.AUTHORS")$ii   )  )),
+      x[ , .(author)] %>%
+    is.element_validator(v = data.table(variable = "author",
+    set = list(dbq(q = "SELECT initials ii FROM AUTHORS")$ii   )  ), db = 'RUFFatSEEWIESEN'),
 
-  
 
-    x[ , .(gps_id)]   %>% 
-    interval_validator( v = data.table(variable = "gps_id",    lq = 1, uq = 13),     
-    "GPS ID not in use" ),
-
-    x[, .(gps_point)]  %>% 
-    interval_validator( v = data.table(variable = "gps_point", lq = 1, uq = 999),     
-    "GPS waypoint is over 999?" ),
-
-    # Entry would be duplicate
-
-    # TODO combo_validator
-   
 
     # Entry should be within specific interval
-    x[, .(tarsus)]     %>% 
-    interval_validator( v = data.table(variable = "tarsus", lq = 20, uq = 24 ),   
+    x[, .(tarsus)]     %>%
+    interval_validator( v = data.table(variable = "tarsus", lq = 20, uq = 24 ),
     "Measurement out of typical range" ),
 
-    x[, .(culmen)] %>% 
-    interval_validator( v = data.table(variable = "culmen",     lq = 20, uq = 25 ),   
+    x[, .(culmen)] %>%
+    interval_validator( v = data.table(variable = "culmen",     lq = 20, uq = 25 ),
     "Measurement out of typical range" ),
 
-    x[, .(weight)]     %>% 
-    interval_validator( v = data.table(variable = "weight",   lq = 40, uq = 72 ),   
+    x[, .(weight)]     %>%
+    interval_validator( v = data.table(variable = "weight",   lq = 40, uq = 72 ),
     "Measurement out of typical range" )
 
 
